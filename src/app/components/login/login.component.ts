63fad37b395f2e20484d61fdd151283338d330e1
OgutuJoseph <onyango.joseph@yahoo.com>
2023-05-18 11:08:25 +0300

    Sign up and login pages

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash"
+  constructor() {  }
+
+  ngOnInit(): void {
+
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password"
+  }
+}
