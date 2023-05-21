aef38ffef8af5acd8f7ad828c23932a0c63df6c3
OgutuJoseph <onyango.joseph@yahoo.com>
2023-05-21 14:17:20 +0300

    Implement toastr notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgToastModule } from 'ng-angular-popup'
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
@@ -20,7 +21,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 import ValidateForm from 'src/app/helpers/validateform';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -16,7 +17,12 @@ export class LoginComponent {
   eyeIcon: string = "fa-eye-slash";
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private auth : AuthService, private router : Router) {  }
+  constructor(
+    private fb: FormBuilder, 
+    private auth : AuthService, 
+    private router : Router,
+    private toast: NgToastService
+  ) {  }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -39,12 +45,14 @@ export class LoginComponent {
       this.auth.login(this.loginForm.value)
       .subscribe({
         next: (res) => {
-          alert(res.message);
+          // alert(res.message);
+          this.toast.success({ detail: "Success", summary: res.message, duration: 5000 })
           this.loginForm.reset();
           this.router.navigate(['dashboard']);
         },
         error: (err) => {
-          alert(err?.error.message)
+          // alert(err?.error.message)
+          this.toast.error({ detail: "Error", summary: "Invalid username or password", duration: 5000 })
         }
       })
     } else {
